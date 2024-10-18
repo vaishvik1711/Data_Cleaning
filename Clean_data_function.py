486c3c5fdfd518d665f21f9e45d105fa8f2ee36d
vaishvik1711 <168612160+vaishvik1711@users.noreply.github.com>
2024-10-18 11:24:27 -0400

    Clean_data_function.py

--- Clean_data_function.py
+++ Clean_data_function.py
@@ -1,18 +1,17 @@
 ##Python Function to clean the data
+##import
 import pandas as pd
 import os
-
+##Global Variables
 data_path = ''
 data_name = ''
 
-
+##Defning the function
 def data_cleaning_master(data_path , data_name):
-    
     if not os.path.exists(data_path):
         print("Please enter correct file")
-    
+    ## Print the file format
     else:
-        
         if data_path.endswith('.csv'):
                 print('Dataset is CSV File')
                 print('\n')
@@ -24,10 +23,10 @@ def data_cleaning_master(data_path , data_name):
                 data = pd.read_excel(data_path)
         else:
             print("Unknown File Format")
-            
+    ## Print shape of the file
     print(f'Dataset contain {data.shape[0]} rows and {data.shape[1]} columns')
     print('\n')
-    
+    ## Find duplicates
     total_duplicates = data.duplicated().sum()
     
     if total_duplicates > 0:
@@ -43,37 +42,30 @@ def data_cleaning_master(data_path , data_name):
     total_duplicates = data.duplicated().sum()
     print(f'Now duplicate values are {total_duplicates}')
     print('\n')
-    
-    
+   
     #########   find missing values     #########
     
     total_na_values = data.isnull().sum().sum()
     na_columns = data.isnull().sum()
     
-    
     print(f'Data has total {total_na_values} NA Values')
     print('\n')
-    
-    
-    
-    
     columns = data.columns
-    
+    ## Remove NA Values and Alter int and float columns
     for col in columns:
         if data[col].dtype in (int, float):
             data[col].fillna(data[col].mean(), inplace = True)
         else:
             data.dropna(subset=col, inplace = True)
-            
+    ## save the file in excel
     print(f'Data is cleaned. Number of rows is {data.shape[0]} and columns are {data.shape[1]}')
     data.to_excel(f'cleaned_{data_name}.xlsx', index = None)
     print('\n')
     print('Data is saved')
-        
+## call the function
 if __name__ == "__Clean_data_function__":
     data_path = input("Please enter Dataset Path:")
     data_name = input("Please enter Dataset Name:")
-    
     
     data_cleaning_master(data_path, data_name)
 
